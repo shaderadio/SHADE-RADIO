7d121eecd0ddc27edee513cc36f0715b808e771d
shaderadio <nayastarkmarley@gmail.com>
2026-02-26 20:57:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    ⚠️ Incrémente CACHE_VERSION à chaque déploiement GitHub
       pour forcer la mise à jour chez tous les utilisateurs
    ============================================================ */
-const CACHE_VERSION = 'v22';
+const CACHE_VERSION = 'v23';
 const CACHE_NAME    = 'shade-radio-' + CACHE_VERSION;
 
 /* Fichiers à mettre en cache lors de l'installation */
