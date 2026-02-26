9706244f5d9c08ff4dd7f498dc34c8666d3f7cea
shaderadio <nayastarkmarley@gmail.com>
2026-02-26 03:23:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
+// Écouter l'événement 'push' envoyé par Supabase
+self.addEventListener('push', function(event) {
+  if (event.data) {
+    const data = event.data.json();
+    const options = {
+      body: data.body,
+      icon: 'assets/logo.JPG', // Assure-toi que le chemin est correct
+      badge: 'assets/logo.JPG',
+      vibrate: [100, 50, 100],
+      data: {
+        url: 'https://shaderadio.github.io/'
+      }
+    };
 
+    event.waitUntil(
+      self.registration.showNotification(data.title || 'SHADE RADIO', options)
+    );
+  }
+});
+
+// Ouvrir le site quand on clique sur la notification
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
